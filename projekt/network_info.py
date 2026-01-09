e80ba833664a80a1be15c085e9ea08d311f182dd
Six10J <sixten00j@gmail.com>
2026-01-09 10:08:05 +0100

    La till början av projektet, version rubrik, import socket och skriver ut hostname

--- projekt/network_info.py
+++ projekt/network_info.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+#Rubrik av programmet visar vilken version programmet är i 
+print ("")
+print ("--- network_info v 0.1 ---")
+print ("")
+
+#För att fastställa att programmet kan arbeta med nätverks information
+import socket
+
+#Skriver ut nätverks infromation
+print("Hostname: " + socket.gethostname())
