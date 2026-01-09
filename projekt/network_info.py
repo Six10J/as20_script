e2fc3145e701f33618b2f8a1e2e30966e7eebc67
Six10J <sixten00j@gmail.com>
2026-01-09 15:19:05 +0100

    La till kod som skriver ut mac addresen

--- projekt/network_info.py
+++ projekt/network_info.py
@@ -4,6 +4,8 @@
 import socket
 #Gör så att programmet kan köra CLI komandon
 import os
+#Impoterar maskinvara info
+import re, uuid
 
 #Rubrik av programmet visar vilken version programmet är i 
 print ("")
@@ -19,6 +21,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 print("IPv4 address: " + s.getsockname()[0])
 s.close()
+
+#Hämtar mac addressen och skriver ut den lånat från https://www.geeksforgeeks.org/python/extracting-mac-address-using-python/ (2026-01-09)
+mac = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+print("MAC address: " + mac)
 
 #Test av att köra CLI komandon
 print ("")
